07424307c4527d3598877e1a43318bb03dde81a8
Shamiul Mowla <sham.mowla@gmail.com>
2022-10-09 18:42:05 -0400

    Update Saucelabs configuration to match new parameters.

--- karma.saucelabs.conf.js
+++ karma.saucelabs.conf.js
@@ -1,5 +1,4 @@
 const karmaSauceLauncher = require("karma-sauce-launcher");
-
 const karmaConfig = require("./karma.conf");
 
 module.exports = config => {
@@ -7,49 +6,45 @@ module.exports = config => {
   const customLaunchers = {
     sl_chromeW3C: {
       base: "SauceLabs",
-      browserName: 'firefox',
-      browserVersion: '105',
-      platformName: 'Windows 10',
-      acceptInsecureCerts: true,
-      'sauce:options': {
-        recordVideo: false,
-        videoUploadOnPass: false,
-        recordScreenshots: false,
-        recordLogs: false,
-      }
-    },
-    sl_firefoxW3C: {
-      base: "SauceLabs",
-      browserName: 'chrome',
-      browserVersion: 'latest',
-      platformName: 'Windows 10',
-      acceptInsecureCerts: true,
-      'sauce:options': {
-        recordVideo: false,
-        videoUploadOnPass: false,
-        recordScreenshots: false,
-        recordLogs: false,
-      }
+      browserName: "chrome",
+      browserVersion: "latest",
+      platform: "Windows 11"
     },
     sl_safariW3C: {
       base: "SauceLabs",
-      browserName: 'safari',
-      browserVersion: '16',
-      platformName: 'macOS 12',
-      acceptInsecureCerts: true,
-      'sauce:options': {
-        recordVideo: false,
-        videoUploadOnPass: false,
-        recordScreenshots: false,
-        recordLogs: false,
+      browserName: "safari",
+      browserVersion: "latest",
+      platform: "macOS 11.00"
+    },
+    sl_firefoxW3C: {
+      base: "SauceLabs",
+      browserName: "firefox",
+      platformName: "Windows 11",
+      browserVersion: "latest",
+      "sauce:options": {
+        screenResolution: "1280x1024",
+        seleniumVersion: "3.141.59",
+        tunnelIdentifier: process.env.JOB_NUMBER
       }
+    },
+    sl_edgeW3C: {
+      base: "SauceLabs",
+      browserName: "microsoftedge",
+      browserVersion: "latest",
+      platform: "Windows 11"
     }
   };
 
   config.set({
     browsers: Object.keys(customLaunchers),
     customLaunchers,
-
+    concurrency: 10,
+    colors: true,
+    sauceLabs: {
+      screenResolution: "800x600",
+      build: `GH #${process.env.BUILD_NUMBER} (${process.env.BUILD_ID})`,
+      tunnelIdentifier: process.env.JOB_NUMBER
+    },
     plugins: [
       "karma-jasmine",
       "karma-coverage",
